c87a2613c81f7f4e9fc898bd26b7fa20a2c29905
tiagoruivo10 <tiagosantarim2017@gmail.com>
2026-03-27 11:23:27 -0300

    Committing the Outlet application for improved route functionality.

---
+++ devburguer-interface/src/layouts/UserLayout/index.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+import { Footer, Header } from '../../components';
+
+export function UserLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
--- devburguer-interface/src/routes/index.jsx
+++ devburguer-interface/src/routes/index.jsx
@@ -1,7 +1,5 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
 import {
   Cart,
   Checkout,
@@ -11,45 +9,21 @@ import {
   Menu,
   Register,
 } from '../containers';
+import { UserLayout } from '../layouts/UserLayout';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: (
-      <>
-        <Header />
-        <Home />
-        <Footer />
-      </>
-    ),
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
-  {
-    path: '/cadastro',
-    element: <Register />,
-  },
-  {
-    path: '/cardapio',
-    element: (
-      <>
-        <Header />
-        <Menu />
-      </>
-    ),
-  },
-  {
-    path: '/carrinho',
-    element: <Cart />,
-  },
-  {
-    path: '/checkout',
-    element: <Checkout />,
-  },
-  {
-    path: '/complete',
-    element: <CompletePayment />,
-  },
-]);
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<UserLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cardapio" element={<Menu />} />
+        <Route path="/carrinho" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/complete" element={<CompletePayment />} />
+      </Route>
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Register />} />
+    </Routes>
+  );
+}
