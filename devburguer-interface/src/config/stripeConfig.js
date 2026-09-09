9ca1cea9efcbfd6c5338756457595a09e564c232
tiagoruivo10 <tiagosantarim2017@gmail.com>
2026-09-09 16:05:35 -0300

    feat: suporte a VITE_STRIPE_PUBLIC_KEY via variavel de ambiente

--- devburguer-interface/src/config/stripeConfig.js
+++ devburguer-interface/src/config/stripeConfig.js
@@ -1,7 +1,8 @@
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(
-  'pk_test_51TF0nyLnGYzPpJF67ufP7DmoxiQNRQbwjZ1WQvhvwPK7mVce3QwlxgnrcyfrbiAXQHZ9bS3NgJao8jPUsU5By0u600FNCuQS1G',
+  import.meta.env.VITE_STRIPE_PUBLIC_KEY ||
+    'pk_test_51TF0nyLnGYzPpJF67ufP7DmoxiQNRQbwjZ1WQvhvwPK7mVce3QwlxgnrcyfrbiAXQHZ9bS3NgJao8jPUsU5By0u600FNCuQS1G',
 );
 
 export default stripePromise;
